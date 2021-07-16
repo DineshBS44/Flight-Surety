6b0144a806594a95781341634618bba172653eea
Dinesh B S <dineshbs444@gmail.com>
2021-07-16 17:58:49 +0530

    Update truffle configurations

--- truffle-config.js
+++ truffle-config.js
@@ -3,20 +3,18 @@ var mnemonic =
   "gospel interest income funny bind unit village guilt erode drip trigger skirt";
 // Mnemonic from ganache-cli
 
-
 module.exports = {
   networks: {
     development: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: "*",
-      gas: 9999999,
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
   },
   compilers: {
     solc: {
-      version: "^0.8.6",
+      version: "0.8.6",
     },
   },
 };
+
